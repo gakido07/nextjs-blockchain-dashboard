ab9b65c6c8eb5d322194b69f6fa3b4be3a5cff8c
Ekara Daniel <daniel.ekara@yahoo.com>
2024-04-15 15:44:28 +0100

    added block list component

--- src/features/blocks/block-list/block-list.tsx
+++ src/features/blocks/block-list/block-list.tsx
@@ -3,16 +3,24 @@ import styles from "./block-list.module.scss";
 import { BlockData } from "@/common/types";
 import BlockSvg from "@/assets/block-svgrepo-com.svg";
 import { DashboardListSkeleton } from "@/features/dashboard/dashboard-list/dashboard-list";
-import { getLastNBlocks } from "@/lib/web3";
+import { getBlocks, web3 } from "@/lib/web3";
 import { ListIconWrapper } from "@/components/list-icon-wrapper/list-icon-wrapper";
+import Link from "next/link";
+import { hideWalletAddress } from "@/common/util";
+import { useState } from "react";
+import { BlockModal } from "@/features/blocks/block-modal/block-modal";
 
 interface BlockListProps {
   blocks?: BlockData[];
   loading?: boolean;
 }
 
-export function BlockList({ blocks, loading = true }: BlockListProps) {
-  getLastNBlocks(10);
+export function BlockList({ blocks, loading }: BlockListProps) {
+  const [blockInFocus, setBlockInFocus] = useState<BlockData>(null);
+  getBlocks({
+    page: 1,
+    pageSize: 5,
+  });
   return (
     <div className={styles["block-list"]}>
       {loading &&
@@ -21,19 +29,45 @@ export function BlockList({ blocks, loading = true }: BlockListProps) {
           .map((_, index) => <DashboardListSkeleton key={index} />)}
       {!loading &&
         Array.isArray(blocks) &&
-        blocks.map(block => <Block key={block.hash} block={block} />)}
+        blocks.map(block => (
+          <Block
+            setBlockInFocus={setBlockInFocus}
+            key={block.hash}
+            block={block}
+          />
+        ))}
+      <BlockModal
+        blockInFocus={blockInFocus}
+        onClose={() => setBlockInFocus(null)}
+      />
     </div>
   );
 }
 
 interface BlockProps {
   block: BlockData;
+  setBlockInFocus?: (block: BlockData) => void;
 }
 
-function Block({}: BlockProps) {
+function Block({ block, setBlockInFocus }: BlockProps) {
   return (
-    <Flex className={styles.block}>
-      <ListIconWrapper></ListIconWrapper>
+    <Flex onClick={() => setBlockInFocus(block)} className={styles.block}>
+      <div className={styles["block-number"]}>
+        <ListIconWrapper>
+          <BlockSvg />
+        </ListIconWrapper>
+        <div>
+          <h6>{block?.number?.toString()}</h6>
+          <p>{block?.timestamp?.toString()}</p>
+        </div>
+      </div>
+      <div className={styles["block-miner"]}>
+        <p>Recipient: {hideWalletAddress(block?.miner)}</p>
+        <p>{block?.transactions?.length} txns</p>
+      </div>
+      <div className={styles["block-amount"]}>
+        {web3.utils.fromWei(block?.gasUsed?.toString(), "ether")}
+      </div>
     </Flex>
   );
 }
